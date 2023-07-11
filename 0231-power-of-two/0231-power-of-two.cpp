01ae4ec3fc14a49b9680d568ebc9f79cd2ed4b14
Ayushi Singhal <92862556+AyushiSinghal14@users.noreply.github.com>
2023-07-11 06:54:53 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (81.18%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -4,16 +4,37 @@ public:
         
     //30 is because the range of integer values is uptil 2^31 - 1 and 31 will not be considered because 1 is subtracted from it
         
-        for(int i = 0; i <= 30; i++)
-        {
-            int ans = pow(2,i);
-            
-            if(ans == n)
-            {
-                return true;
-            }
-        }
+        //BRUTE FORCE
         
-        return false;
+//         for(int i = 0; i <= 30; i++)
+//         {
+//             int ans = pow(2,i);
+            
+//             if(ans == n)
+//             {
+//                 return true;
+//             }
+//         }
+        
+//         return false;
+        
+        
+        //OPTIMIZED APPROACH
+        
+        // int temp = 1;
+        // while(n != temp && temp < INT_MAX/2)
+        // {
+        //     temp*=2;
+        // }
+        // return n == temp;
+        
+        
+        //MOST EFFICIENT APPROACH
+        
+        if(n<=0)
+        {
+            return false;
+        }
+        return (n & (n-1)) == 0;
     }
 };
