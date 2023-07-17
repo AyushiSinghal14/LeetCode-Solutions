a5ef898c89701a6b6cd760e88e2f79996247ec65
Ayushi Singhal <92862556+AyushiSinghal14@users.noreply.github.com>
2023-07-17 21:25:56 +0530

    Added solution - LeetHub

---
+++ First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		    // Code here
+		    string ans = "";
+        int n = A.length();
+        
+        vector<vector<int>> freq(26, vector<int>(2)); // store freq and idx
+        
+        for (int i = 0; i < n; i++)
+        {
+            char ch = A[i];
+            freq[ch - 'a'][0]++; // increase frequency
+            freq[ch - 'a'][1] = i; // store the idx
+            
+            int res_ch = -1;
+            int min_idx = INT_MAX;
+            
+            // Get the character with freq 1 and lowest idx (first non-repeating)
+            for (int j = 0; j < 26; j++) {
+                if (freq[j][0] == 1 && freq[j][1] < min_idx) {
+                    min_idx = freq[j][1];
+                    res_ch = j; // Store its corresponding character
+                }
+            }
+            
+            if (res_ch == -1) {
+                ans += '#';
+            } else {
+                ans += (char)(res_ch + 'a');
+            }
+        }
+        
+        return ans;
+		}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string A;
+		cin >> A;
+		Solution obj;
+		string ans = obj.FirstNonRepeating(A);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
