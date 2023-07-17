66f252c85f61c257c124e4bc853aa85c0c3b312d
Ayushi Singhal <92862556+AyushiSinghal14@users.noreply.github.com>
2023-07-17 21:27:13 +0530

    Added solution - LeetHub

--- First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
+++ First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
@@ -10,22 +10,21 @@ class Solution {
 		    string ans = "";
         int n = A.length();
         
-        vector<vector<int>> freq(26, vector<int>(2)); // store freq and idx
+        vector<vector<int>> f(26, vector<int>(2));
         
         for (int i = 0; i < n; i++)
         {
             char ch = A[i];
-            freq[ch - 'a'][0]++; // increase frequency
-            freq[ch - 'a'][1] = i; // store the idx
+            f[ch - 'a'][0]++;
+            f[ch - 'a'][1] = i;
             
             int res_ch = -1;
             int min_idx = INT_MAX;
             
-            // Get the character with freq 1 and lowest idx (first non-repeating)
             for (int j = 0; j < 26; j++) {
-                if (freq[j][0] == 1 && freq[j][1] < min_idx) {
-                    min_idx = freq[j][1];
-                    res_ch = j; // Store its corresponding character
+                if (f[j][0] == 1 && f[j][1] < min_idx) {
+                    min_idx = f[j][1];
+                    res_ch = j;
                 }
             }
             
