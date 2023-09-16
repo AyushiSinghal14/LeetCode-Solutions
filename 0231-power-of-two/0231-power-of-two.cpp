f0b599deb633b5cb35d53d802df4d7af8c948fe7
Ayushi Singhal <92862556+AyushiSinghal14@users.noreply.github.com>
2023-09-16 21:14:19 +0530

    Time: 3 ms (27.21%), Space: 6.4 MB (18.89%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,39 +2,15 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         
-    //30 is because the range of integer values is uptil 2^31 - 1 and 31 will not be considered because 1 is subtracted from it
-        
-        //BRUTE FORCE
-        
-//         for(int i = 0; i <= 30; i++)
-//         {
-//             int ans = pow(2,i);
-            
-//             if(ans == n)
-//             {
-//                 return true;
-//             }
-//         }
-        
-//         return false;
-        
-        
-        //OPTIMIZED APPROACH
-        
-        // int temp = 1;
-        // while(n != temp && temp < INT_MAX/2)
-        // {
-        //     temp*=2;
-        // }
-        // return n == temp;
-        
-        
-        //MOST EFFICIENT APPROACH
-        
-        if(n<=0)
+        for(int i = 0; i <= 30; i++)
         {
-            return false;
+            int ans = pow(2, i);
+            if(ans == n)
+            {
+                return true;
+            }
         }
-        return (n & (n-1)) == 0;
+        return false;
+        
     }
 };
