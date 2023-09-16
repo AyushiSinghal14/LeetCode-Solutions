a443458de7c662a36d05c51b3a2941d67b0f5f60
Ayushi Singhal <92862556+AyushiSinghal14@users.noreply.github.com>
2023-09-16 19:33:43 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (6.26%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
     int reverse(int x) {
-        // int ans = 0;
-        // while (x != 0)
-        // {
-        //     int digit = x%10;
-        //     ans = (ans*10)+digit;
-        //     x = x/10;
-        // }
-        // return ans;
-        
-        // (in the above code, there is some value which becomes bigger than the range of integer after multiplying it with 10. this is why we need to specify the range on the basis of condition)
-    
-        int ans = 0;
+         int ans = 0;
         while(x != 0)
         {
             int digit = x%10;
